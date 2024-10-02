db8efa472a5c37796afa0beebc2ec2869caca3be
Ilshat Ablyuzin <revan931@yandex.ru>
2024-10-02 23:47:35 +0500

    implementation with Stream

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -26,6 +26,7 @@ public class UserMealsUtil {
         mealsTo.forEach(System.out::println);
 
 //        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -41,8 +42,9 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO Implement by streams
-        return null;
+        return meals.stream().filter(meal -> TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime))
+                .map(meal -> new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
+                        isExcessCalPerDay(meals, meal.getDateTime(), caloriesPerDay))).toList();
     }
 
     private static boolean isExcessCalPerDay(List<UserMeal> meals, LocalDateTime day, int caloriesPerDay) {
