b712db57dd611d0d0add5d9e100ec7908afce9bf
Ilshat Ablyuzin <revan931@yandex.ru>
2024-10-03 02:20:39 +0500

    isExceesCalPerDay() with forEach

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,9 +6,7 @@ import ru.javawebinar.topjava.model.UserMealWithExcess;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -19,7 +17,7 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2020, Month.MARCH, 31, 20, 0), "Ужин", 410)
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
 
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
@@ -42,13 +40,21 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        return meals.stream().filter(meal -> TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime))
+        return meals.stream()
+                .filter(meal -> TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime))
                 .map(meal -> new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
-                        isExcessCalPerDay(meals, meal.getDateTime(), caloriesPerDay))).toList();
+                        isExcessCalPerDay(meals, meal.getDateTime(), caloriesPerDay)))
+                .toList();
     }
 
     private static boolean isExcessCalPerDay(List<UserMeal> meals, LocalDateTime day, int caloriesPerDay) {
-        int sumCalPerDay = meals.stream().filter(meal -> meal.getDateTime().getDayOfMonth() == day.getDayOfMonth()).mapToInt(UserMeal::getCalories).sum();
+        int sumCalPerDay = 0;
+        for (UserMeal meal : meals) {
+            if (meal.getDateTime().toLocalDate().compareTo(day.toLocalDate()) == 0) {
+                int calories = meal.getCalories();
+                sumCalPerDay += calories;
+            }
+        }
         return sumCalPerDay >= caloriesPerDay;
     }
 }
